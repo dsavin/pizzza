4bf7fcdf41e2b3b5b4b13d5f80c3fd5d9f11fc6f
Dmitry Savin <savin.dmitry@gmail.com>
2014-03-14 11:43:02 +0200

    Remove debug

--- src/Main/Bundle/Controller/CommentsController.php
+++ src/Main/Bundle/Controller/CommentsController.php
@@ -230,9 +230,6 @@ class CommentsController extends Controller
 
         $array = array();
 
-        var_dump($request->get('comment_id'));
-        exit;
-
         if ($request->isXmlHttpRequest()) {
 
             $em = $this->getDoctrine()->getManager();
